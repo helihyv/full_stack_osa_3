89b602bceee0fd674ca9583e2d1b5734fd5406a4
Heli Hyvättinen <heli.hyvattinen@netti.fi>
2018-08-20 16:15:48 +0300

    Tehtävä 3.15
    Tehtävä 3.15 valmis.
    (Ei toimi, koska tietokannan salasana puuttuu.)

--- index.js
+++ index.js
@@ -81,10 +81,6 @@ app.delete('/api/persons/:id',(request,response) => {
   response.status(204).end()
 })
 
-const generateId = () => {
-  return Math.floor(Math.random() * 10000000) + 1
-}
-
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
@@ -97,13 +93,6 @@ app.post('/api/persons', (request, response) => {
 
   else if (body.name.length === 0) {
     error = 'no name provided'
-  }
-
-  else if (persons.some((person => {
-    return person.name === body.name
-  })))
-  {
-    error = 'name must be unique'
   }
 
   if (body.number === undefined) {
@@ -123,15 +112,19 @@ app.post('/api/persons', (request, response) => {
     }
 
 
-
-  const person = {
+  const person = new Person ({
     name: body.name,
-    number: body.number,
-    id: generateId()
-  }
+    number: body.number
+  })
 
-  persons = persons.concat(person)
-  response.json(person)
+  person
+    .save()
+    .then(savedPerson => {
+      response.json(Person.formatPerson(savedPerson))
+    })
+    .catch(error => {
+      console.log(error)
+    })
 })
 
 const PORT = process.env.PORT || 3001
